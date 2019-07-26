56210a3c5b5bb54cf233f5d4b7e1cc133ac2f566
Maham Siddiqui <49103875+MahamSiddiqui@users.noreply.github.com>
2019-07-26 23:03:43 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+//Q1
+// for (let i = 0; i < 9; i++) {
+
+//     console.log(i +1);
+
+// }
+
+//Q2
+
+// for (let x = 2; x <= 2; x++) {
+//     console.log("Table of 2");
+//     for (let i = 1; i <= 10; i++) {
+//         let output = x * i;
+//         console.log(x + " * " + i + " = " + output);
+//     }
+// }
+
+
+//Q3
+// for (let x = 2; x <= 9; x++) {
+//     console.log("Table of " + x);
+//     for (let i = 1; i <= 10; i++) {
+//         let output = x * i;
+//         console.log(x + " * " + i + " = " + output);
+//     }
+//     console.log("");
+// }
+
+//Q4 iska json print karwana ahi.......
+// let students = [
+//     {
+//         name: "Amna",
+//         hobbies: ["eating", "cooking"]
+//     },
+//     {
+//         name: "Daniyal",
+//         hobbies: ["arts", "shopping"]
+//     },
+//     {
+//         name: "Fahad",
+//         hobbies: ["coding", "cooking"]
+//     },
+//     {
+//         name: "Hajra",
+//         hobbies: ["sleep", "reading"]
+//     }
+// ];
+// for (let i = 0; i < students.length; i++) {
+//     console.log("Hobbies of " + students[i].name);
+//     console.log( students[i].hobbies);
+//     console.log("");
+
+// }
+
+//Q5
+
+for (let i = 1; i < 10; i++) {
+
+    console.log(i + 1);
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
